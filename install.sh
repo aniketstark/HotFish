dd496cde25e8b4517fffe6e168d5fdb6210606fc
Aniket Stark <31062805+aniketstark@users.noreply.github.com>
2024-01-20 18:52:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 apt update -y && apt install update -y
 pkg install root-repo -y
-pkg install zip root-repo python git iptables 
+pkg install zip root-repo python git iptables -y
 pip install requests wheel termcolor
